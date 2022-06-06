34262e01cb20c0e8be4c8694a891d30422170014
Dzejkobik007 <kuba.rybnicek@gmail.com>
2022-06-06 10:35:26 +0200

    Add dcComics.py

---
+++ dev-prg/DCComics/dcComics.py
@@ -0,0 +1,24 @@
+class DCComics:
+    def __init__(self, name : str, born : int, weapons : list, bestActorName : str):
+        self.__name = str(name)
+        self.__born = int(born)
+        self.__weapons = list(weapons)
+        self.__bestActorName = str(bestActorName)
+    def setName(self,name : str):
+        self.__name = name
+    def setBorn(self,born : int):
+        self.__born = born
+    def setBestActorName(self,bestActorName : str):
+        self.__bestActorName = bestActorName
+    def getName(self):
+        return self.__name
+    def getBorn(self):
+        return self.__born
+    def getBestActorName(self):
+        return self.__bestActorName
+    def addWeapon(self, weapon : str):
+        self.__weapons.append(weapon)
+    def removeWeapon(self, weapon : str):
+        self.__weapons.remove(weapon)
+    def getWeapons(self):
+        return self.__weapons
