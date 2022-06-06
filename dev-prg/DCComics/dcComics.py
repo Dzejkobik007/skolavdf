b9dc54425b63292e9733ad523956da17de0b1f71
Dzejkobik007 <kuba.rybnicek@gmail.com>
2022-06-06 10:43:40 +0200

    Update dcComics.py

--- dev-prg/DCComics/dcComics.py
+++ dev-prg/DCComics/dcComics.py
@@ -22,3 +22,19 @@ class DCComics:
         self.__weapons.remove(weapon)
     def getWeapons(self):
         return self.__weapons
+
+dc = DCComics("test", 1, ["weapon1","weapon2","weapon3","weapon4"], "Bestactor")
+print("Name:",dc.getName())
+print("BestActorName:",dc.getBestActorName())
+print("Born:",dc.getBorn())
+print("Weapons:",dc.getWeapons())
+dc.setName("comicsname")
+dc.setBorn(10)
+dc.setBestActorName("Actor2000")
+dc.addWeapon("gigaweapon")
+dc.removeWeapon("weapon2")
+print("=================================================================")
+print("Name:",dc.getName())
+print("BestActorName:",dc.getBestActorName())
+print("Born:",dc.getBorn())
+print("Weapons:",dc.getWeapons())
